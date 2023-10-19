21bafa609582a1ab383859776c8fde415bbe7ca6
I-Bio <116032943+I-Bio@users.noreply.github.com>
2023-10-19 22:22:57 +0300

    Revert "Upload"
    This reverts commit 1fac97cfc71fe96fe93e84f900a4f7741eb1203c.

--- main/context_processors.py
+++
@@ -1,4 +0,0 @@
-from .models import *
-
-def global_models(request):
-    return { 'subjects_all': Subject.objects.all(), 'units_all': Unit.objects.all() }
--- static/content_bank/fileForm.js
+++
@@ -1,8 +0,0 @@
-let filename = $("#filename")[0];
-let textId = $("#name")[0];
-
-$("#file").change(
-	function (){
-		filename.value = this.files.length ? this.files[0].name : '';
-	}
-);
