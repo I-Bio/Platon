1ba72663acceaf6b99b00d262611ee8d40fd386b
truk808 <116074308+truk808@users.noreply.github.com>
2023-11-18 14:37:33 +0300

    начал делать таблицу оценок для преподавателя

--- main/views/tutor/student/gradeCardList.py
+++ main/views/tutor/student/gradeCardList.py
@@ -3,9 +3,18 @@ from django.shortcuts import render
 from django.views import View
 
 from main.mixins.tutorRequired import TutorRequiredMixin
+from main.models import StudentGroup, Subject
 
 
 class ShowGradeCardList(TutorRequiredMixin, View):
     def get(self, request, group_id, subject_id):
-        return HttpResponse("asasasa")
-        # return  render(request, 'tutor/grade_card.html')
+        info_group = StudentGroup.objects.filter(id=group_id).first().name
+        info_subject = Subject.objects.filter(id=subject_id).first()
+        print(info_subject.name)
+        context = {
+        'info_group_name' : info_group,
+        'info_subject' : info_subject,
+
+
+        }
+        return render(request, 'tutor/grade_card.html', context=context)
