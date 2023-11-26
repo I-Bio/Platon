0efa995caf841aa5b4775f18d8ceba8abb54a9f4
olegator111111 <oleg.korn1enko@yandex.com>
2023-11-26 17:24:45 +0300

    Починил отображение левой панели

--- main/views/tutor/task/showUncompletedTasks.py
+++ main/views/tutor/task/showUncompletedTasks.py
@@ -10,11 +10,10 @@ class showUncompletedTasks(View):
 
         group_obj = GroupCheck.objects.filter(usser_id=request.user.pk)
 
-
         if group_obj.count() == 0:
             raise PermissionDenied()
 
-        if group_obj.get(main_task_id=main_task_id) == None:
+        if group_obj.filter(main_task_id=main_task_id).exists() == False:
             main_task_id = group_obj.first().main_task_id.pk
 
         group_check = GroupCheck.objects.filter(usser_id=request.user.pk)
@@ -28,7 +27,6 @@ class showUncompletedTasks(View):
         for name_task in group_check_all:
             arr_name_tasks.append(name_task.main_task_id)
 
-
         group_check = group_check.filter(main_task_id=main_task_id)
         if group_check.count() == 0:
             raise PermissionDenied()
@@ -39,21 +37,18 @@ class showUncompletedTasks(View):
         list_whom_can_check = group_check.first().user_check_id
         user_info = User.objects.filter(groups=request.user.groups.all().first(), id__in=list_whom_can_check)
 
-
         task_user = UserTask.objects.filter(group_id=request.user.groups.all().first().pk, main_task_id=main_task_id)
         list_checked = []
         for task in task_user:
             list_checked.append(task.user_id.pk)
 
-
-
         context = {
-            'arr_name_tasks' : arr_name_tasks,
-            'user_info' : user_info,
-            'name_group' : name_group,
-            'name_task' : name_task,
-            'list_checked' : list_checked,
+            'arr_name_tasks': arr_name_tasks,
+            'user_info': user_info,
+            'name_group': name_group,
+            'name_task': name_task,
+            'list_checked': list_checked,
 
-            'main_task_id' : main_task_id,
+            'main_task_id': main_task_id,
         }
         return render(request, 'tutor/tasks_to_be_checked.html', context=context)
