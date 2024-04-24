8390987b551ba12f70b30bc1d916d9df535df996
olegator111111 <gipigao081@gmail.com>
2024-04-24 23:08:01 +0300

    Сделал ссылки на задания в таблице проверяющих

--- main/templatetags/get_user.py
+++ main/templatetags/get_user.py
@@ -1,9 +1,24 @@
 from django import template
-from main.models import User
+from main.models import User, GroupCheck, Subject, Unit
 
 register = template.Library()
+
 
 @register.filter
 def get_user(user_id):
     user = User.objects.get(id=user_id)
     return f"{user.first_name} {user.last_name}"
+
+
+@register.filter
+def get_subject(main_task_id):
+    unit = Unit.objects.get(tasks=main_task_id)
+
+    subject = Subject.objects.filter(unit=unit).first()
+
+    return int(subject.pk)
+
+@register.filter
+def get_unit(main_task_id):
+
+    return int(main_task_id.pk)
