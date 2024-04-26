74ec62968358923aa5a854458954709042df3623
truk808 <116074308+truk808@users.noreply.github.com>
2024-04-26 20:46:35 +0300

    Жил в древней яЖил в древней японии один мудрец и была у него своя идзакая (таверна). Это заведение было известно тем, что в стенах его никогда не происходили драки. и однажды молодой ронин, чтобы узнать в чем заключается секрет мудреца, решил проследить за ним. Ронин встал рано утром спрятался в кустах напротив идзакаи и стал наблюдать: через пару минут к стенам заведения подошел мудрец, он высып мешок риса, сел в позу лотоса и стал чательно собирать его обратно в своей мешок. Спустя пару часов, он собрал рис, встал и только тогда открыл заведение. Так ронин следил за мудрецом месяц и каждый день владелец таверны делал одно и тоже действие. В один день, ронин не выдержал и после того как мудрец зашел в идзакаю подбежал к нему и спросил: “О, мудрец, ваше заведение известно всем благодаря спокойствию и умиротворению в его стенах. В чем ваш секрет? Не сочтите за грубость, но на протяжении месяца я следил за вами и я не понимаю, зачем вы высыпаете и собираете рис каждый день?”.  Тот же тяжело вздохнул, приподнял брови и сказал: «А тебя это ебать не должно» и уебал мешком риса по ронину.понии один мудрец и была у него своя идзакая (таверна). Это заведение было известно тем, что в стенах его никогда не происходили драки. и однажды молодой ронин, чтобы узнать в чем заключается секрет мудреца, решил проследить за ним

--- main/views/student/studentManualView.py
+++ main/views/student/studentManualView.py
@@ -1,7 +1,21 @@
+import os
+
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
+from Platon import settings
 
 
 class StudentManualView(View):
     def get(self, request):
         return render(request, 'students/manual_for_student.html')
+
+    def post(self, request):
+        file_path = os.path.join(settings.MEDIA_ROOT, 'file/stud_manual.docx')
+        with open(file_path, 'rb') as file:
+            response = HttpResponse(file.read(),
+                                    content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+            response['Content-Disposition'] = 'attachment; filename="manual.docx"'
+            return response
+
