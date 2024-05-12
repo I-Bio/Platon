be216b9c32a9cee925dfbd94801932a32f39eb56
I-Bio <116032943+I-Bio@users.noreply.github.com>
2024-05-12 15:24:05 +0300

    Reapply "Жил в древней яЖил в древней японии один мудрец и была у него своя идзакая (таверна). Это заведение было известно тем, что в стенах его никогда не происходили драки. и однажды молодой ронин, чтобы узнать в чем заключается секрет мудреца, решил проследить за ним. Ронин встал рано утром спрятался в кустах напротив идзакаи и стал наблюдать: через пару минут к стенам заведения подошел мудрец, он высып мешок риса, сел в позу лотоса и стал чательно собирать его обратно в своей мешок. Спустя пару часов, он собрал рис, встал и только тогда открыл заведение. Так ронин следил за мудрецом месяц и каждый день владелец таверны делал одно и тоже действие. В один день, ронин не выдержал и после того как мудрец зашел в идзакаю подбежал к нему и спросил: “О, мудрец, ваше заведение известно всем благодаря спокойствию и умиротворению в его стенах. В чем ваш секрет? Не сочтите за грубость, но на протяжении месяца я следил за вами и я не понимаю, зачем вы высыпаете и собираете рис каждый день?”.  Тот же тяжело вздохнул, приподнял брови и сказал: «А тебя это ебать не должно» и уебал мешком риса по ронину.понии один мудрец и была у него своя идзакая (таверна). Это заведение было известно тем, что в стенах его никогда не происходили драки. и однажды молодой ронин, чтобы узнать в чем заключается секрет мудреца, решил проследить за ним"
    This reverts commit 5bceecdcb8eb9e773a4d73a98b8c45586c609fb7.

--- main/views/tutor/teacherManualView.py
+++ main/views/tutor/teacherManualView.py
@@ -1,7 +1,20 @@
+import os
+
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
+from Platon import settings
 
 
 class TeacherManualView(View):
     def get(self, request):
         return render(request, 'tutor/manual_for_teacher.html')
+
+    def post(self, request):
+        file_path = os.path.join(settings.MEDIA_ROOT, 'file/manual.docx')
+        with open(file_path, 'rb') as file:
+            response = HttpResponse(file.read(),
+                                    content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+            response['Content-Disposition'] = 'attachment; filename="manual.docx"'
+            return response
